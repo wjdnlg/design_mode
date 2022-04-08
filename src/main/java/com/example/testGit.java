befed3b7bdc5e7656a48f6a0f668d7059bf43d5d
wjdnlg <wjdnlg@163.com>
2022-04-08 10:23:10 +0800

    新需求

---
+++ src/main/java/com/example/testGit.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zjd
+ * @version 1.0
+ * @date 2022/4/8 10:18
+ */
+@Slf4j
+public class testGit {
+    public static void main(String[] args) {
+        log.info("新需求");
+    }
+}
