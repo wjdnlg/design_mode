75cd96092560b3b71177cbe126a1463e1ed729e7
wjdnlg <wjdnlg@163.com>
2022-04-08 10:37:17 +0800

    新需求

---
+++ src/main/java/com/example/NewBug.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author zjd
+ * @version 1.0
+ * @date 2022/4/8 10:35
+ */
+public class NewBug {
+    public static void main(String[] args) {
+        System.out.println("我是大bug分支");
+    }
+}
